aa53cb7c941aceaa80b5b0bc1762e4c6f8562f87
werfa1 <35898158+werfa1@users.noreply.github.com>
2023-01-04 18:17:49 -0800

    Add project files.

---
+++ BulkyBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBooksWeb.Data
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
